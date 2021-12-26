942d4ef5726ca96d0fd96f89cbafcf77cdc69ca2
ismail7979 <isma7979@mail.ru>
2021-12-26 05:00:46 +0300

    12

--- api/minus.php
+++ api/minus.php
@@ -10,7 +10,7 @@ include ('/var/www/html/params.php');
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 //$sql = "INSERT INTO log(Number1,Number2,Result,UserID,Timestamp) VALUES (?,?,?,?,NOW())";
 //$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonym')";
-$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$user')";
+$sql = "INSERT INTO log(Number1,Number2,Result,UserID,Timestamp) VALUES($x,$y,$z,'$user',now()";
 mysqli_query($conn,$sql);
 mysqli_close($conn);
 echo($z);
